a5d2fb4f61e0c6427921a022094d0fd18a18e01d
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2020-12-19 00:18:01 -0600

    Refactor get_image_url

--- periodicos.rb
+++ periodicos.rb
@@ -5,36 +5,20 @@ def get_date_path(date)
   return date.strftime("%Y/%m/%d")
 end
 
-def get_el_universal_url(date)
+# newspaper is one of: el-universal, excelsior, la-jornada, milenio, reforma
+def get_image_url(newspaper, date)
   date_path = get_date_path(date)
 
-  return "https://tar.mx/periodicos-mexico/dl/#{date_path}/el-universal_t.jpg"
-end
-
-def get_la_jornada_url(date)
-  date_path = get_date_path(date)
-
-  return "https://tar.mx/periodicos-mexico/dl/#{date_path}/la-jornada_t.jpg"
-end
-
-def get_milenio_url(date)
-  date_path = get_date_path(date)
-
-  return "https://tar.mx/periodicos-mexico/dl/#{date_path}/milenio_t.jpg"
-end
-
-def get_reforma_url(date)
-  date_path = get_date_path(date)
-
-  return "https://tar.mx/periodicos-mexico/dl/#{date_path}/reforma_t.jpg"
+  return "https://tar.mx/periodicos-mexico/dl/#{date_path}/#{newspaper}_t.jpg"
 end
 
 start = Date.parse("2020-01-01")
 stop = Date.today
 
 start.step(stop, 1).each do |date|
-  image_url = get_milenio_url(date)
-  local_image_path = "./assets/milenio/#{date.to_s}.jpg"
+  newspaper = "milenio"
+  image_url = get_image_url(newspaper, date)
+  local_image_path = "./assets/#{newspaper}/#{date.to_s}.jpg"
 
   begin
     open(image_url) do |image|
